0618f97b9b33cc54d57b7b51995d16bdae0d7235
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2024-01-01 22:18:46 +0000

    feat(configure somethings): configure somethings

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,12 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Boilerplate-angular' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Boilerplate-angular');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
