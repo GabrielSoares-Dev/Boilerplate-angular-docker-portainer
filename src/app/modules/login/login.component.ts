94fd73e36135c9bc1abaf5c15564472f414fb5ff
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2024-01-01 23:45:33 +0000

    feat(stop): stop

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent implements OnInit, OnDestroy {
+  form!: FormGroup;
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl(''),
+    });
+  }
+
+  onSubmit() {}
+
+  protected resetForm() {
+    this.form.reset();
+  }
+
+  ngOnDestroy(): void {
+    this.resetForm();
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '@shared/components/button/button.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ButtonComponent],
-  imports: [CommonModule],
-  exports: [ButtonComponent],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  exports: [ButtonComponent, FormsModule, ReactiveFormsModule],
 })
 export class SharedModule {}
