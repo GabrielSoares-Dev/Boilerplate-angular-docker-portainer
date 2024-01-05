26cb4d5d8bcfacec83d89760f3eca1955c672cb5
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2024-01-04 22:14:50 -0300

    feat(initialize interceptor): intialize interceptor

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '@services/user/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  constructor(private readonly UserService: UserService) {}
+  name = '';
+
+  ngOnInit() {
+    const name = this.UserService.getCurrent().name;
+
+    this.name = name;
+  }
+}
