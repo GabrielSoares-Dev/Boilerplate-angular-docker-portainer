df12aba43a2693b8f6cfbe1e1e52f11bb8c2631f
Gabriel Soares Maciel <gabriel.maciel@terceirizadoraizen.com>
2024-01-08 13:19:45 -0300

    style(code style): code style

--- src/app/modules/home/home.component.spec.ts
+++ src/app/modules/home/home.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '@shared/shared.module';
+import { LogoutDataSource } from './datasources/logout.datasource';
+import { LogoutUseCase } from './usecases/logout.usecase';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
@@ -8,7 +11,8 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent],
+      imports: [HomeComponent, CommonModule, SharedModule],
+      providers: [LogoutDataSource, LogoutUseCase],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
